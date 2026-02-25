996ec2178f476790a55adcc4ef0c3ff9e4aaa3d3
Shubham Maurya <151072431+ShubhamMaurya7@users.noreply.github.com>
2026-02-25 14:16:08 +0530

    Add files via upload

---
+++ MlOps-unit1/main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+# Load dataset
+df = pd.read_csv("data.csv")
+
+# Print first 5 rows
+print("First 5 rows:")
+print(df.head())
+
+# Basic statistics
+print("\nDataset Info:")
+print(df.info())
+
+print("\nStatistical Summary:")
+print(df.describe())
+
+print("\nChecking for missing values:")
+print(df.isnull().sum())
